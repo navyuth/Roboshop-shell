1479ead9b195c42b152a1806d706576c000d9283
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-16 13:41:27 +0530

    Created servers script files

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,10 @@
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl restart nginx
+systemctl enable nginx
+systemctl start nginx
+
+# Roboshop Config is not copied
+# If any command is failed in middle, we need to stop the script(by default it will proceed to the next command)
