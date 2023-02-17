acddfa2234d8495925c1cdcef18678a5607a71d1
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-17 13:40:20 +0530

    Roboshop Components

--- frontend.sh
+++ frontend.sh
@@ -3,8 +3,10 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
 
-# Roboshop Config is not copied
+cp /configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemctl restart nginx
+
+# Roboshop Config is not copied - It is done in the config file adding the DNS records from route 53
 # If any command is failed in middle, we need to stop the script(by default it will proceed to the next command)
