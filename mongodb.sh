3eda460745b484c08e66fcba5515ff36890554dc
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-17 13:50:04 +0530

    Roboshop Components

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 
-cp /configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
