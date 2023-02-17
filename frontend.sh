51b8568200794cb54e5cdad68540019a3dfa7eb5
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-17 14:27:04 +0530

    Roboshop Components

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,23 @@
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35mRemoving the old content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[35mDownloading frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[35mExtracting downloaded frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[35mCopying Nginx Config for Roboshop\e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35mEnabling nginx service\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35mStarting Nginx service\]e[0m"
 systemctl restart nginx
 
 # Roboshop Config is not copied - It is done in the config file adding the DNS records from route 53
