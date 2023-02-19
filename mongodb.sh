64355a6b98c23fb4c5225209727685b06a6234c2
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-19 20:11:21 +0530

    Update MongoDB Listen address"

--- mongodb.sh
+++ mongodb.sh
@@ -8,6 +8,7 @@ yum install mongodb-org -y &>>${log_file}
 
 print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 print_head "Enable MongoDB service"
 systemctl enable mongod &>>${log_file}
 
