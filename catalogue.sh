e74e3244e9846c432fa229278a5af2a0c684a7b0
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-21 12:37:52 +0530

    manually added status_check $? in catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -21,28 +21,36 @@ cd /app
 
 print_head "Extracting App content"
 unzip /tmp/catalogue.zip &>>${log_file}
+status_check $?
 
 print_head "Installing NodeJs dependencies"
 npm install &>>${log_file}
+status_check $?
 
 print_head "Copying SystemD service file"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+status_check $?
 
 print_head "Reload  SystemD"
 systemctl daemon-reload &>>${log_file}
+status_check $?
 
 print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${log_file}
+status_check $?
 
 print_head "Start Catalogue Service"
 systemctl start catalogue &>>${log_file}
+status_check $?
 
 print_head "Copy MongoDB Repo File"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
 
 print_head "Install Mongo Client"
 yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
 
 print_head "Load Schema"
 mongo --host mongodb.devopscourse.online </app/schema/catalogue.js &>>${log_file}
-
+status_check $?
--- mongodb.sh
+++ mongodb.sh
@@ -2,17 +2,21 @@ source common.sh
 
 print_head "Setup MongoDB repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
 
 print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
+status_check $?
 
 print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status_check $?
 
 print_head "Enable MongoDB service"
 systemctl enable mongod &>>${log_file}
+status_check $?
 
 print_head "Start MongoDB service"
 systemctl restart mongod &>>${log_file}
-
+status_check $?
 # update /etc/mongod.conf file 127.0.0.1 with 0.0.0.0
