0dd29ebfbceb1f19580e6e1a18795f9d067cc283
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-21 13:01:35 +0530

    added if condition for application directory

--- catalogue.sh
+++ catalogue.sh
@@ -16,7 +16,9 @@ fi
 status_check $?
 
 print_head "Create application directory"
-mkdir /app &>>${log_file}
+if [ ! -d /app ]; then
+  mkdir /app &>>${log_file}
+fi
 status_check $?
 
 print_head "Delete old content"
