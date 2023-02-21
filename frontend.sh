afe5405ab26d6b3c3381b30916462bbdc4c71a61
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-21 11:54:23 +0530

    added echo $? in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -2,25 +2,32 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+echo $?
 
 print_head "Removing the old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
 print_head "Downloading frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Extracting downloaded frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Copying Nginx Config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+echo $?
 
 print_head "Enabling nginx service"
 systemctl enable nginx &>>${log_file}
+echo $?
 
 print_head "Starting Nginx service"
 systemctl restart nginx &>>${log_file}
+echo $?
 
 # Roboshop Config is not copied - It is done
 # If any command is failed in middle, we need to stop the script(by default it will proceed to the next command)
