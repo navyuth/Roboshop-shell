6543a53bc6858c4968db938c0e94be2ecd0e7973
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-22 16:27:58 +0530

    created rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
 if [ -z "${roboshop_app_password}" ]; then
-  echo -e "\e[31mMissing MySQL Root Password Argument\e[0m"
+  echo -e "\e[31mMissing Roboshop App Password Argument\e[0m"
   exit 1
 fi
 
