d62aecc82835efa4184c0a953d783610a5209ecf
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-23 12:09:12 +0530

    made changes in schema_setup with -dev IP addresses

--- common.sh
+++ common.sh
@@ -87,7 +87,7 @@ schema_setup() {
     status_check $?
 
     print_head "Load Schema"
-    mysql -h mysql.devopscourse-dev.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+    mysql -h mysql-dev.devopscourse.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
     status_check $?
 
   fi
